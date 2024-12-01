b03b3ae3726c48c1a24049091ead53025bea0bbf
Pavel Druzhinin <p.a.druzhinin@tbank.ru>
2024-12-01 23:13:05 +0300

    day1_2

--- day1/day1.go
+++ day1/day1.go
@@ -73,3 +73,45 @@ func abs(f, s int) int {
 	}
 	return s - f
 }
+
+func Second() {
+	f, err := os.Open("day1/data.txt")
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(f)
+	l, r := []int{}, []int{}
+	for scanner.Scan() {
+		res := strings.Split(scanner.Text(), "   ")
+		fd, err := strconv.Atoi(res[0])
+
+		if err != nil {
+			panic(err)
+		}
+		sd, err := strconv.Atoi(res[1])
+		if err != nil {
+			panic(err)
+		}
+
+		l = append(l, fd)
+		r = append(r, sd)
+	}
+
+	rs := map[int]int{}
+	for _, v := range r {
+		if c, ok := rs[v]; ok {
+			rs[v] = c + 1
+		} else {
+			rs[v] = 1
+		}
+	}
+
+	total := 0
+	for _, v := range l {
+		if c, ok := rs[v]; ok {
+			total += v * c
+		}
+	}
+
+	fmt.Println(total)
+}
--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "github.com/paveldroo/advent-of-code-2024/day1"
 
 func main() {
-	day1.First()
+	day1.Second()
 }
